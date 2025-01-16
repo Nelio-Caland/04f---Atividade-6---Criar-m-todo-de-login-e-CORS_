7375f9949bd3c094a1e5cdfd3f97a566a3037d39
Nelio-Caland <nelio.calland@gmail.com>
2025-01-15 21:53:37 -0300

    04e - Atividade 5 - Criar CRUD de usuários_commit

--- obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb5144bc60705a4dd8da46af0ff3c4f8c019a274")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9303562e14410da1a47510b5474998ba804c9154")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
